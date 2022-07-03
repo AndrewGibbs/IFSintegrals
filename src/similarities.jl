db4bcd8b9b3ed6a563aa1fc353c87593526e7a76
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2022-07-03 21:38:21 +0100

    still some changes in similarities...?

--- src/similarities.jl
+++ src/similarities.jl
@@ -31,7 +31,6 @@ end
 sim_map(s::Similarity, x::Union{Real,AbstractVector{<:Real}}) = s.rA*x + s.δ
 sim_map_inv(s::Similarity, x::Union{Real,AbstractVector{<:Real}}) = s.rA/(x-s.δ)
 
-<<<<<<< HEAD
 function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:AbstractMatrix}
     Achain = [s.A*IFS[m].A/s.A for m=1:length(IFS)]
     rAchain = [IFS[m].r*Achain[m] for m=1:length(IFS)]
@@ -40,15 +39,6 @@ end
 
 # below could just be rs
 function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:Real}
-=======
-# function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:AbstractMatrix}
-#     Achain = [s.A*IFS[m].A/s.A]
-#     rAchain = s.r*Achain
-#     return [Similarity{V,M}(IFS[m].r, (-rAchain+s.Ar+I)*IFS[m].δ, Achain, rAchain) for m=1:length(IFS)]
-# end
-
-function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}}
->>>>>>> master
     return [Similarity{V,M}(IFS[m].r, (I-IFS[m].rA)*s.δ + s.rA*IFS[m].δ, IFS[m].A, IFS[m].rA) for m=1:length(IFS)]
 end
 
