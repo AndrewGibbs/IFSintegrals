b854b8bfde9e9ad42e43c9b5496b3f805861a726
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2022-07-12 13:04:40 +0100

    Created and tidied sing_homog_ints

---
+++ src/singular_homogenous_integrals.jl
@@ -0,0 +1,80 @@
+"""
+    eval_green_double_integral(Γ::Union{Attractor,SubAttractor}, t::Float64, h::Float64; μ::Vector{Float64} = Γ.weights)
+
+Approximates the integral ∫_Γ∫_Γ Φ_t(x,y) dμ₁(y)dμ₂(x), where Φ_t is the Green's function for
+the n-dimensional Laplace problem, and the integrals are with respect to Hausdorff measure.
+
+The optional argument μ₂ will default to μ₁, which is the probability weights assigned to the measure of Γ.
+But this can be specified manually, if it is required that both integrals are over a different measure.
+"""
+function eval_green_double_integral(Γ::SelfSimilarFractal, t::Real, h::Real; μ₂::Vector{Float64} = [0.0])
+
+    if isa(Γ,Attractor)
+        μ₁ = Γ.weights
+    else
+        μ₁ = Γ.attractor.weights
+    end
+    if μ₂ == [0.0]
+        μ₂ = μ₁
+    end
+    M = length(Γ.IFS)
+    for μ = [μ₁,μ₂]
+        length(μ)!=M ? error("μ must be a vector containing the same length as the IFS") : Nothing
+        sum(μ) ≈ 1 ? Nothing : error("μ must sum to one")
+    end
+
+    log_sum = 0.0
+    scale = 1.0
+    smooth_integrals = 0.0
+
+    for m=1:M
+        scale -= Γ.IFS[m].r^(-t)*μ₁[m]*μ₂[m]
+        if t == 0.0
+            log_sum += Γ.measure^2 * μ₁[m]*μ₂[m] * log(Γ.IFS[m].r)
+        end
+        Γm = Γ[m]
+
+        # can exploit symmetry of double sum
+
+        for n=(m+1):M
+            if m!=n
+                X1, X2, W = barycentre_rule(Γm,Γ[n],h)
+                smooth_integrals += 2*W'* Φₜ.(t,X1,X2)
+            end
+        end
+    end
+    return (smooth_integrals + log_sum)/scale
+end
+
+"""
+    eval_green_single_integral_fixed_point(Γ::Union{Attractor,SubAttractor}, t::Float64, h::Float64, n::Int64)
+    
+Approximates the integral ∫_Γ Φ_t(x,y) dμ(x), where Φ_t is the Green's function for
+the n-dimensional Laplace equation, and the integrals are with respect to Hausdorff measure.
+"""
+function eval_green_single_integral_fixed_point(Γ::SelfSimilarFractal, t::Real, h::Real, n::Int64)
+    if isa(Γ,Attractor)
+        μ = Γ.weights
+    else
+        μ = Γ.attractor.weights
+    end
+    ηₙ = fixed_point(Γ.IFS[n]) # get fixed point, convert to standard vector type
+    Φₜ_(x) = Φₜ(t,x,ηₙ)
+    M = length(Γ.IFS)
+    if t == 0.0
+        log_sum = Γ.measure*μ[n]*log(Γ.IFS[n].r)
+    else
+        log_sum = 0.0
+    end
+    smooth_integrals = 0.0
+    scale = 1.0 - (Γ.IFS[n].r^-t*μ[n])
+    for m=1:M
+        if m!=n
+            x, w = barycentre_rule(Γ[m],h)
+            smooth_integrals += w'* Φₜ_.(x)
+        end
+    end
+
+    return (smooth_integrals + log_sum)/scale
+
+end
