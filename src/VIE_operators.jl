3a161c75876713c09f58bf31ea88b14def321a64
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2023-01-30 11:49:39 +0000

    Fixed warning in volume_potential

--- src/VIE_operators.jl
+++ src/VIE_operators.jl
@@ -5,13 +5,13 @@ function volume_potential(density::Projection{V,M}, k::Real; h_quad::Float64=0.1
 
     dims = density.domain.spatial_dimension
     dims != density.domain.Hausdorff_dimension ? error("Hausdorff dimension must equal spatial dimension") : nothing
-    if dims == 2
-        ϕ(R::Float64) = HelhmoltzGreen2D(k,R)
-    elseif dims == 3
-        ϕ(R::Float64) = HelhmoltzGreen3D(k,R)
-    else
-        error("Volume potential doesn't make sense for dimension not equal to 2 or 3.")
-    end
+    # if dims == 2
+        ϕ2(R::Float64) = HelhmoltzGreen2D(k,R)
+    # elseif dims == 3
+        ϕ3(R::Float64) = HelhmoltzGreen3D(k,R)
+    # else
+    #     error("Volume potential doesn't make sense for dimension not equal to 2 or 3.")
+    # end
 
     function Nϕ(x::Vector{Float64})
         R = zeros(Float64,vec_length)
@@ -21,7 +21,17 @@ function volume_potential(density::Projection{V,M}, k::Real; h_quad::Float64=0.1
             end
         end
         R = Vector{Float64}(sqrt.(R))
-        return transpose(W::Vector{Complex{Float64}})*(ϕ.(R))
+
+        val = zero(ComplexF64)
+        if dims == 2
+            val = transpose(W::Vector{Complex{Float64}})*(ϕ2.(R))
+        elseif dims == 3
+            val = transpose(W::Vector{Complex{Float64}})*(ϕ3.(R))
+        else
+            error("Volume potential doesn't make sense for dimension not equal to 2 or 3.")
+        end
+
+        return val
     end
     return Nϕ
 end
