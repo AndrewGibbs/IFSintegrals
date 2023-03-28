5f45c7a998d6b82e5eccb10dadd566779a409f0a
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2023-03-28 22:54:27 +0100

    Added similarity syntax

--- docs/src/makeIFS.md
+++ docs/src/makeIFS.md
@@ -1,9 +1,9 @@
 An iterated function system is a set of $M$ affine contraction maps, or _similarities_.
 
 Each similarity is of the form:
-$$
-s_m(x)=r_mA_mx + \delta_m,\quad x\in\mathbb{R}^n,
-$$
+
+$s_m(x)=r_mA_mx + \delta_m,\quad x\in\mathbb{R}^n,$
+
 where $r_m\in(0,1)$ is the contraction factor, $A_m\in\R^{n\times n}$ is a rotation matrix, and $\delta\in\R^{n}$ is a translation vector.
 
 ```@docs
@@ -11,11 +11,20 @@ Similarity
 ```
 
 The iterated function system may be interpreted as a set of these maps, $\{s_m\}_{m=1}^M$. The _attractor_ $\Gamma$ is the unique non-empty compact set which satisfies
-$$
-\Gamma = \bigcup_{m=1}^M s_m(\Gamma).
-$$
 
-The attractor $\Gamma$ will be the support of our measure. There are other properties, such as diameter and Hausdorff dimension, which describe this type.
+$\Gamma = S(\Gamma):=\bigcup_{m=1}^M s_m(\Gamma).$
+
+We can construct the map $S$ defined above as follows.
+```
+s₂ = Similarity(1/3,2/3)
+S = [s₁,s₂]
+x = rand()
+S(x) # applies the map S to the point x
+```
+This is the IFS for the Cantor Set.
+
+
+The attractor $\Gamma$ will be the support of our measure $\mu$. There are other properties, such as diameter and Hausdorff dimension, which describe this type.
 
 ```@docs
 SelfSimilarMeasure
--- src/similarities.jl
+++ src/similarities.jl
@@ -1,9 +1,9 @@
-""""
+"""
     struct Similarity{V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}}
         r::Float64 # contraction
         δ::V # translation
-        A::M # rotation
-        rA::M # contraction * rotation
+        A::M # rotation/reflection
+        rA::M # contraction * rotation/reflection
     end
 
 Constructs a similarity map. 
@@ -24,6 +24,7 @@ struct Similarity{V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}}
     rA::M # contraction * rotation
 end
 
+# constructor
 function Similarity(r::Real, δ::Union{Vector{<:Real},Real}, rotation::Union{AbstractMatrix,Real}=I(length(δ)))
     # θ = Float64(θ)
     r = Float64(r)
@@ -52,40 +53,26 @@ function Similarity(r::Real, δ::Union{Vector{<:Real},Real}, rotation::Union{Abs
     return Similarity(r,static_δ,A,r*A)
 end
 
-# function Similarity(r::Real, δ::Union{Vector{<:Real},Real}, θ=0::Real)
-#     ndims = length(δ)
-#     if θ == 0
-#         return Similarity(r, δ, I(ndims))
-#     else
-#         if ndims != 2
-#             error("If rotation is to be determined from a single angle, similarity must be two-dimensional")
-#         end
-#         A = [cos(θ) -sin(θ); sin(θ) cos(θ)]
-#         return Similarity(r, δ, A)
-#     end
-# end
+# define similarity as a map
+sim_map(s::Similarity, x) = s.rA*x + s.δ
+(s::Similarity{V,M})(x::V) where {V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}} = sim_map(s,x)
+#inverse map
+sim_map_inv(s::Similarity, x) = s.rA/(x-s.δ)
 
-sim_map(s::Similarity, x::Union{Real,AbstractVector{<:Real}}) = s.rA*x + s.δ
-sim_map_inv(s::Similarity, x::Union{Real,AbstractVector{<:Real}}) = s.rA/(x-s.δ)
-
+# similarity acting on an IFS
 function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:AbstractMatrix}
     Achain = [s.A*IFS[m].A/s.A for m=1:length(IFS)]
     rAchain = [IFS[m].r*Achain[m] for m=1:length(IFS)]
     return [Similarity{V,M}(IFS[m].r, (I-rAchain[m])*s.δ + s.rA*IFS[m].δ, Achain[m], rAchain[m]) for m=1:length(IFS)]
 end
+# simplified case without rotation
+function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:Real}
+    return [Similarity{V,M}(IFS[m].r, (I-IFS[m].rA)*s.δ + s.rA*IFS[m].δ, IFS[m].A, IFS[m].rA) for m=1:length(IFS)]
+end
 
-function (s::Similarity{V,M})(x::Union{Real,AbstractVector{<:Real}}) where {V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}}
-    return sim_map(s,x)
-end 
-
+# composition of two similarities
 ∘(s₁::Similarity{V,M},s₂::Similarity{V,M}) where {V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}} = Similarity(s₁.r*s₂.r, s₁.δ+s₁.rA*s₂.δ, s₁.A*s₂.A, s₁.rA*s₂.rA)
 
-"""   
-r::Float64 # contraction
-δ::V # translation
-A::M # rotation
-rA::M # contraction * rotation
-"""
 function sim_comp(IFS::Vector{Similarity{V,M}}, m::Vector{Int64}) where {V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}}
     s = IFS[m[1]]
         for j = 2:length(m)
@@ -94,12 +81,8 @@ function sim_comp(IFS::Vector{Similarity{V,M}}, m::Vector{Int64}) where {V<:Unio
     return s
 end
 
-# below could just be rs
-function sim_map(s::Similarity{V,M}, IFS::Vector{Similarity{V,M}}) where {V<:Union{Real,AbstractVector}, M<:Real}
-    return [Similarity{V,M}(IFS[m].r, (I-IFS[m].rA)*s.δ + s.rA*IFS[m].δ, IFS[m].A, IFS[m].rA) for m=1:length(IFS)]
-end
-
-function full_map(S::Vector{Similarity{V,M_}},X::Vector{V_}) where {V<:Union{Real,AbstractVector}, V_<:Union{Real,AbstractVector}, M_<:Union{Real,AbstractMatrix}}
+# union of similarities acting on a set of points, returning a set of points:
+function full_map(S::Vector{Similarity{V,M_}},X::Vector{V_}) where {V<:AbstractVector{<:Real}, V_<:AbstractVector{<:Real}, M_<:Union{Real,AbstractMatrix}}
     d = length(X)
     M = length(S)
     N = length(S[1].δ)
@@ -111,9 +94,24 @@ function full_map(S::Vector{Similarity{V,M_}},X::Vector{V_}) where {V<:Union{Rea
     end
     return Y
 end
+function full_map(S::Vector{Similarity{V,M_}},X::Vector{V_}) where {V<:Real, V_<:Real, M_<:Real}
+    d = length(X)
+    M = length(S)
+    Y = zeros(V_,d*M)
+    for nₓ = 1:d
+        for m=1:M
+            Y[d*(m-1)+nₓ] = sim_map(S[m], X[nₓ])
+        end
+    end
+    return Y
+end
 
-full_map(S::Vector{Similarity{V,M_}},X::V) where {V<:Union{Real,AbstractVector}, M_<:Union{Real,AbstractMatrix}} = (full_map(S,[X]))[1]
+# versions acting on a single vector/value
+full_map(S::Vector{Similarity{V,M}},X::V_) where {V<:AbstractVector, V_<:AbstractVector, M<:Union{Real,AbstractMatrix}} = (full_map(S,[X]))
+full_map(S::Vector{Similarity{V,M}},X::V_) where {V<:Real, V_<:Real, M<:Real} = (full_map(S,[X]))
 
-# full_map(S::Array{Similarity},x::Vector{<:Real}) where N = full_map(S,reshape(x,length(x),1))
+# shorthand for everything, which appears quite natural
+(S::Vector{Similarity{V,M_}})(X) where {V<:Union{Real,AbstractVector}, M_<:Union{Real,AbstractMatrix}} = full_map(S,X)
 
+# define the fixed point of the map
 fixed_point(s::Similarity{V,M}) where {V<:Union{Real,AbstractVector}, M<:Union{Real,AbstractMatrix}} = (I-s.rA) \ s.δ
