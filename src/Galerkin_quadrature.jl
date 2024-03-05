e919c61e1a8af0b27f24e7fe3585f14b9213c325
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-03-05 15:03:03 +0000

    fixed varyquad bug

--- src/Galerkin_quadrature.jl
+++ src/Galerkin_quadrature.jl
@@ -7,15 +7,16 @@ function get_quad_scales(k::Real, spat_dim::Int64,mesh::Vector{<:SubInvariantMea
     # compute upper and lower bounds for the F in my notes, which is stated above.
     N = length(mesh)
     F_upper = ones(Float16,N,N)
-    F_lower = ones(Float16,N,N)
+    # F_lower = ones(Float16,N,N)
+    F_lower = zeros(Float16,N,N)
     for m_count = 1:N
         Γₘ = mesh[m_count]
         for n_count = m_count:N
             Γₙ = mesh[n_count]
             if n_count!=m_count
-                dist_upper = norm(Γₙ.barycentre-Γₘ.barycentre)
+                dist_upper = norm(Γₙ.barycentre - Γₘ.barycentre)
                 dist_lower = max(dist_upper - Γₙ.diameter - Γₘ.diameter,0)
-                measure_weight = Γₙ.measure*Γₘ.measure
+                measure_weight = 1.0#Γₙ.measure*Γₘ.measure
                 # noting that F_nomeasure is monotonic decreasing in r, can bound as follows:
                 if dist_lower>0
                     F_upper[m_count,n_count] = measure_weight*F_nomeasure(dist_lower, k, spat_dim)
@@ -44,7 +45,8 @@ function get_quad_scales(k::Real, spat_dim::Int64,
     N = length(mesh2)
     M = length(mesh1)
     F_upper = ones(Float16,M,N)
-    F_lower = ones(Float16,M,N)
+    # F_lower = ones(Float16,N,N)
+    F_lower = zeros(Float16,N,N)
 
     for m = 1:M
         Γₘ = mesh1[m]
@@ -52,7 +54,7 @@ function get_quad_scales(k::Real, spat_dim::Int64,
             Γₙ = mesh2[n]
             dist_upper = norm(Γₙ.barycentre-Γₘ.barycentre)
             dist_lower = max(dist_upper - Γₙ.diameter - Γₘ.diameter, 0)
-            measure_weight = Γₙ.measure*Γₘ.measure
+            measure_weight = 1.0#Γₙ.measure*Γₘ.measure
             # noting that F_nomeasure is monotonic decreasing in r, can bound as follows:
             if dist_lower>0
                 F_upper[m,n] = measure_weight*F_nomeasure(dist_lower, k, spat_dim)
